48dd0da379bb701f1e681b3f7409fda38b3f857b
kalanstrauss <kstrau1@saic.edu>
2020-01-30 14:35:55 -0600

    RGB Rozin Mirro Sketch

--- docs/code/in_class/day01/01_RozinMirror/scripts/main.js
+++ docs/code/in_class/day01/01_RozinMirror/scripts/main.js
@@ -1,8 +1,7 @@
 let myCapture;
 
 function setup() {
-  let canvas = createCanvas(320, 240);
-  canvas.parent("sketch");
+  createCanvas(320, 240);
   myCapture = createCapture(VIDEO);
   myCapture.hide();
   
@@ -12,61 +11,55 @@ function setup() {
 }
 
 function draw() {
-  background(255);
+  background(0,0,0);
   // fill(random(255),random(255),random(255));
   
   //load pixel data into myCapture object
   myCapture.loadPixels();
   
-  const stepSize = round(constrain(mouseX / 8, 6, 32));
+  const stepSize = round(constrain(mouseX / 8, 12, 32));
   
-  for(let y = 0; y < height; y +=stepSize) {
+  // for(let y = 0; y < height; y +=stepSize) {
+  //   for(let x = 0; x < width; x +=stepSize) {
+  //     const i = y * width + x;
+  //     const darkness = (255 - myCapture.pixels[i * 8]) / 255;
+  //     const radius = stepSize * darkness;
+  //     //blendMode(MULTIPLY);
+  //     fill(0,255,0,127);
+  //     noStroke();
+  //     // push();
+  //     //   translate(width,0);
+  //     //   scale(-1,1);
+  //       circle(x,y,radius,radius);
+  //     //pop();
+      
+  //   }
+  // }
+  
+ for(let y = 0; y < height; y +=stepSize) {
     for(let x = 0; x < width; x +=stepSize) {
       const i = y * width + x;
       const darkness = (255 - myCapture.pixels[i * 8]) / 255;
       const radius = stepSize * darkness;
       //blendMode(MULTIPLY);
-      fill(0,255,127,255);
-      // push();
-      //   translate(width,0);
-      //   scale(-1,1);
-        rect(x,y,radius,radius);
-      //pop();
-      
-    }
-  }
-  
-  for(let y = 0; y < height; y +=stepSize) {
-    for(let x = 0; x < width; x +=stepSize) {
-      const i = y * width + x;
-      const darkness = (255 - myCapture.pixels[i * 8]) / 127;
-      const radius = stepSize * darkness;
-      //blendMode(REPLACE);
-      fill(0,0,255,127);
-      push();
-        translate(width,0);
-        scale(-1,1);
-        rect(x,y,radius,radius);
+      fill(0,255,0,255);
+      noStroke();
+      circle(x,y,radius,radius);
+      push(); // Start a new drawing state
+      strokeWeight(10);
+      fill(255, 0, 0, 255);
+      translate(5, 0);
+      blendMode(ADD);
+      circle(x, y, radius, radius); // Middle circle
+      pop();
+      push();// Restore original state
+      fill(0,0,255,255);
+      translate(-5,0);
+      blendMode(ADD);
+      circle(x, y, radius, radius); // Right circle
       pop();
       
-      
     }
-  }
-  
-  
-  for(let y = 0; y < height; y +=stepSize) {
-    for(let x = 0; x < width; x +=stepSize) {
-      const i = y * width + x;
-      const darkness = (255 - myCapture.pixels[i * 8]) / 127;
-      const radius = stepSize * darkness;
-      //blendMode();
-      fill(255,0,0,85);
-      push();
-        translate(width * .9 ,1);
-        scale(-2,1);
-        rect(x,y,radius,radius);
-      pop();
-    }
-  }
+ }
   
 }
